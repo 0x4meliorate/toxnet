132b719d250f8a9a0448c09e4f0d882ff047db83
Ameliorate <35051260+0x4meliorate@users.noreply.github.com>
2022-08-14 23:42:42 +0100

    Update config.go

--- net/config.go
+++ net/config.go
@@ -1,7 +1,7 @@
 package net
 
 // Admin public keys
-var Admins = []string{"99C3111EC0A66672418FCFD9113DB6A1A0F5B54500461B2E8D5A6D4DF2071705"}
+var Admins = []string{"PLACE_YOUR_TOX_ID_RIGHT_HERE"}
 
 // C2 Private Key
-var Tox_key = "./server.data"
+var Tox_key = "./c2.data"
